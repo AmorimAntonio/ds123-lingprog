4849e311d7aaaaa14b2d3850a3e938253c60f2d4
AmorimAntonio <amorimnetoo02@gmail.com>
2025-08-10 18:18:51 -0300

    ex 02

---
+++ lista_01/ex02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){   
+    //declarando
+    float n1, n2;
+
+    //entrada dos valores
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &n1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%f", &n2);
+
+    //calculando
+    printf ("%.2f + %.2f = %.2f\n", n1, n2, n1+n2);
+    printf ("%.2f - %.2f = %.2f\n", n1, n2, n1-n2);
+    printf ("%.2f * %.2f = %.2f\n", n1, n2, n1*n2);
+    if (n2 == 0) {
+        printf("Nao e possivel dividir por zero. ");
+    } else {
+        printf("%.2f / %.2f = %.2f\n", n1, n2, n1 / n2);
+    }
+
+    return 0;
+}
---
+++ lista_01/ex03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    //declarando
+    int op;
+    
+    //lendo a opção
+    while (op < 0 or op > 3) {
+        printf("===== Escolha =====\n")        
+        printf("===== Escolha =====\n")
+
+    }
+
+
+    return 0;
+}
