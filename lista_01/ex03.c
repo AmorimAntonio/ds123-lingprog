d67c407c18b7156a3ac7484ea5f22571d34a0ab9
AmorimAntonio <amorimnetoo02@gmail.com>
2025-08-11 11:12:14 -0300

    ex03

--- lista_01/ex03.c
+++ lista_01/ex03.c
@@ -2,15 +2,36 @@
 
 int main(){
     //declarando
-    int op;
+    int op = 0;
+    float val, temp, dist, massa;
     
     //lendo a opção
-    while (op < 0 or op > 3) {
-        printf("===== Escolha =====\n")        
-        printf("===== Escolha =====\n")
-
+    while (op < 1 || op > 3) {
+        printf("===== Escolha =====\n");        
+        printf("1. Converter temperatura (C para F)\n2. Converter distancia (m para km)\n3. Converter peso (kg para lb)\n");
+        scanf("%d", &op);
     }
 
+    printf("Digite o valor a ser convertido\n");
+    scanf("%f", &val);
+    
+    //op 1
+    if (op==1) {
+        temp = val*1.8+32;
+        printf("%.2f C = %.2f F", val, temp);
+    }
+
+    //op 2
+    else if (op==2) {
+        dist = val/1000;
+        printf("%.2f m = %.2f km", val, dist);
+    }
+
+    //op 3
+    else {
+        massa = val*2.205;
+        printf("%.2f kg = %.2f lb, aproximadamente.", val, massa);
+    }
 
     return 0;
 }
